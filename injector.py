58f0efa46c7ada23b58abca7d19572f423b6a318
Jeremy Mikkola <jeremy128@gmail.com>
2014-02-27 22:01:56 -0800

    Refactor

--- injector.py
+++ injector.py
@@ -9,19 +9,15 @@ class DuplicateNameException(InjectorException):
 
 class Dependencies(object):
     def __init__(self):
-        self._names_used = set()
+        self._factories = dict()
 
     def _add_item(self, kind, name, value, dependencies):
-        if not name or not isinstance(name, str):
-            raise BadNameException("Bad name: {!r}".format(name))
-        if name in self._names_used:
-            raise DuplicateNameException("Duplicate name: {}".format(name))
         self._names_used.add(name)
 
     def register_value(self, name, value):
         """ Bind a value to a name. The Injector will always return the value as-is.
         """
-        self._add_item('value', name, value, None)
+        self.register_factory(name, lambda: value)
 
     def register_factory(self, name, factory, dependencies=None):
         """ Binds a factory to a name. The injector will call the factory function once
@@ -29,7 +25,12 @@ class Dependencies(object):
 
         The factory will be called with the dependencies (if any listed) as arguments.
         """
-        self._add_item('factory', name, factory, dependencies)
+        if not name or not isinstance(name, str):
+            raise BadNameException("Bad name: {!r}".format(name))
+        if name in self._factories:
+            raise DuplicateNameException("Duplicate name: {}".format(name))
+
+        self._factories[name] = (factory, dependencies)
 
     def build_injector(self):
         return Injector()
