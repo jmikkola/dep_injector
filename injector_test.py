dd717a677929227838e6480458c54fee17307c49
Jeremy Mikkola <jeremy128@gmail.com>
2014-02-27 21:50:54 -0800

    Name field correctly

--- injector_test.py
+++ injector_test.py
@@ -6,27 +6,29 @@ import injector
 
 class DependenciesTest(unittest.TestCase):
     def setUp(self):
-        self.injector = injector.Dependencies()
+        self.dependencies = injector.Dependencies()
 
     def test_can_add_things(self):
-        self.injector.register_value('my value', 123)
-        self.injector.register_factory('my factory', lambda: 1)
+        self.dependencies.register_value('my value', 123)
+        self.dependencies.register_factory('my factory', lambda: 1)
 
     def test_accepts_dependency_lists(self):
-        self.injector.register_factory('my factory', lambda: 1, dependencies=['my value'])
+        self.dependencies.register_factory(
+            'my factory', lambda: 1, dependencies=['my value']
+        )
 
     def test_requires_names(self):
         with self.assertRaises(injector.BadNameException):
-            self.injector.register_value(None, 123)
+            self.dependencies.register_value(None, 123)
         with self.assertRaises(injector.BadNameException):
-            self.injector.register_value(123, 123)
+            self.dependencies.register_value(123, 123)
         with self.assertRaises(injector.BadNameException):
-            self.injector.register_value(object(), 123)
+            self.dependencies.register_value(object(), 123)
 
     def test_disallows_duplicate_names(self):
-        self.injector.register_value('x', 1)
+        self.dependencies.register_value('x', 1)
         with self.assertRaises(injector.DuplicateNameException):
-            self.injector.register_factory('x', lambda: 2)
+            self.dependencies.register_factory('x', lambda: 2)
 
 if __name__ == '__main__':
     unittest.main()
