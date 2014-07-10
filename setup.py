009017b730a021296db0ded53bafb94dc2833139
Jeremy Mikkola <jeremy128@gmail.com>
2014-07-09 22:18:29 -0700

    Add a setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+import setuptools
+
+setuptools.setup(
+    name='dep_injector',
+    version='1.0.0',
+    packages=setuptools.find_packages(),
+    author='Jeremy Mikkola',
+    description='A dependency injector backend',
+    install_requires=[],
+)
